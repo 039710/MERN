52877c77e1bdc8de7c79e0cbda5e2c3d73b4c701
Ahmad Mustain Billah <mustain.bilah@gmail.com>
2021-08-20 13:50:44 +0700

    -

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURI')
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
-const connectDB = async () =>{
+const connectDB = async () => {
   try {
-    await mongoose.connect(db,{
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex : true,
-      useFindAndModify : false,
-    }) 
-    console.log('MongoDB connected')
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB connected");
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
     // to exit if there is error with failure
-    process.exit(1); 
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
